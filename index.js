551ff3ee2f60bfd740554331d895fd92fe058f60
Rogelio Alberto <i@rog.mx>
2016-02-08 12:40:00 +0000

    Fix path on readFileSync

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-var fs = require( "fs" );
-
-fs.readFile( "./me.json", "utf8", function( err, data ) {
-  if ( err ) {
-    return console.log( err );
-  }
-  console.log( data );
-} );
-
+var fs = require('fs')
+module.exports = JSON.parse(fs.readFileSync(__dirname+'/me.json', 'utf8'))
