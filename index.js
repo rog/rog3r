b1af0b4e8df6f20635c71e91e56d0e3a04219469
Rogelio Alberto <i@rog.mx>
2016-02-10 12:40:00 +0000

    Implement ES6 class

--- index.js
+++ index.js
@@ -1,48 +1,14 @@
-var fs = require('fs')
-// var CoolDude = function (options) {
-//   return {
-//     github: function () {
-//       console.log('Github')
-//     },
-//     info: function () {
-//       return JSON.parse(fs.readFileSync(__dirname + '/me.json', 'utf8'))
-//     }
-//   }
-// }
-//
-// var rog3r = new CoolDude()
-// module.exports = rog3r
-var CoolDude = function (opt) {
-  return {
-    auth: function (opt) {
-      console.log('auth', opt)
-    },
+import fs from 'fs'
 
-    pickle: function (cucumber, herbs, vinegar) {
-      console.log('pickle')
-    },
-
-    get: function () {
-      return JSON.parse(fs.readFileSync(__dirname + '/me.json', 'utf8'))
-    }
+class CoolDude {
+  constructor () {
   }
-}
-module.exports = function (method) {
-  (function () {
-    var r = new CoolDude()
-    return (method === undefined) ? r.get() : r.auth()
-  }(method))
-}
-/*
 
-var handler = {
-  get: function (target, name) {
-    return target.hasOwnProperty(name) ? target[name] : 42
+  info () {
+    return JSON.parse(fs.readFileSync(__dirname + '/me.json', 'utf8'))
   }
 }
 
-module.exports = function () {
-  var r = new CoolDude(r, handler)
-  return r
-  // return (method === undefined) ? r.rog() : r.auth()
-} */
+const rog3r = new CoolDude()
+
+console.log(rog3r.info())
