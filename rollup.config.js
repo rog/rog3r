505e2ace2f8c20be7f621655805960969c39333d
Rogelio Alberto <i@rog.mx>
2016-02-16 12:40:00 +0000

    Display FileSize of built
    - Playing with Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,19 @@
 import json from 'rollup-plugin-json'
 import buble from 'rollup-plugin-buble'
 import uglify from 'rollup-plugin-uglify'
+import filesize from 'rollup-plugin-filesize'
 import { minify } from 'uglify-js'
+
+const fsOptions = {
+  format: {bits: true},
+  render: (options, size) => {
+    return `Library built [rog3r] - ${size}\n`
+  }
+}
 
 export default {
   entry: 'src/rog.js',
   format: 'cjs',
-  plugins: [ json(), buble(), uglify({}, minify) ],
+  plugins: [ json(), buble(), uglify({}, minify), filesize(fsOptions) ],
   dest: 'lib/rog.js'
 }
