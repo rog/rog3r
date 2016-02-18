00653d03d3c5f4ba1201379d58fbe2c45ff000f5
Rogelio Alberto <i@rog.mx>
2016-02-18 00:12:00 -0500

    Add fetch

--- src/rog.js
+++ src/rog.js
@@ -1,23 +1,29 @@
+import request from './utils/request'
 const fs = require('fs')
 
 class CoolDude {
   constructor () {
     const info = JSON.parse(fs.readFileSync(__dirname + '/me.json', 'utf8'))
-    this.props = {}
+    this.props = info
     this.props.info = () => { return info }
-    this.props.getTweets = this.twitter
-    this.props.getName = this.name
+    this.props.getTweets = this.getTweets
+    this.props.getName = this.getName
+    this.props.githubActivity = this.githubActivity
     return this.props
   }
 
-  name () {
+  getName () {
     const name = 'Rogelio'
-    const lastName = 'Alberto :D'
+    const lastName = 'Alberto'
     return `${name} ${lastName}`
   }
 
-  twitter () {
+  getTweets () {
     return 'twitter'
+  }
+
+  githubActivity () {
+    return request('https://api.github.com/users/rogr/events')
   }
 }
 const rog3r = new CoolDude()
