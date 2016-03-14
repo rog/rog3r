aafd6418737c8c98c2283ef1bccdd93ad1d6e8c4
Rogelio Alberto <i@rog.mx>
2016-03-14 00:12:00 -0500

    Optimize minify build

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,29 @@
 var gulp = require('gulp')
 var babel = require('gulp-babel')
+var rename = require('gulp-rename')
 var uglify = require('gulp-uglify')
 
 gulp.task('default', ['build'])
 
 gulp.task('babel', function () {
   return gulp.src('src/rog.js')
-  .pipe(babel({
-    presets: ['es2015', 'stage-0'],
-    plugins: ['transform-runtime']
-  }))
-  .pipe(gulp.dest('lib'))
+    .pipe(babel({
+      presets: ['es2015', 'stage-0'],
+      plugins: ['transform-runtime']
+    }))
+    .pipe(gulp.dest('lib'))
 })
 
-gulp.task('copy-me-json', function () {
+gulp.task('copy', function () {
   gulp.src('src/me.json')
-  .pipe(gulp.dest('lib'))
-  .pipe(gulp.dest('lib/min'))
+    .pipe(gulp.dest('lib'))
+  gulp.src('lib/rog.js')
+    .pipe(uglify())
+    .pipe(rename('rog.min.js'))
+    .pipe(gulp.dest('lib'))
 })
 
-gulp.task('min', function () {
-  return gulp.src('src/rog.js')
-  .pipe(babel({
-    presets: ['es2015', 'stage-0'],
-    plugins: ['transform-runtime']
-  }))
-  .pipe(uglify())
-  .pipe(gulp.dest('lib/min'))
-})
-
-gulp.task('build', ['watch', 'babel', 'min', 'copy-me-json'])
+gulp.task('build', ['watch', 'babel', 'copy'])
 
 gulp.task('watch', function () {
   gulp.watch(['src/rog.js'], ['build'])
